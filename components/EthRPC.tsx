4780c8802ecbcdb296035c54b648a11732f96d82
dumikau <44052182+dumikau@users.noreply.github.com>
2022-11-22 15:32:53 +0300

    Update available eth_* methods and aliases

--- components/EthRPC.tsx
+++ components/EthRPC.tsx
@@ -44,49 +44,71 @@ export function EthRPC() {
             </SmartLink>
           </TitleWithSubtitle>
           <p>eth_accounts</p>
-          <p>Filecoin.EthAccounts</p>
           <p>eth_blockNumber</p>
-          <p>Filecoin.EthBlockNumber</p>
-          <p>eth_feeHistory</p>
-          <p>Filecoin.EthFeeHistory</p>
-          <p>eth_getBlockByNumber</p>
-          <p>Filecoin.EthGetBlockByNumber</p>
-          <p>eth_getTransactionByHash</p>
-          <p>Filecoin.EthGetTransactionByHash</p>
-          <p>eth_getTransactionCount</p>
-          <p>Filecoin.EthGetTransactionCount</p>
-          <p>eth_getTransactionReceipt</p>
-          <p>Filecoin.EthGetTransactionReceipt</p>
-          <p>eth_getBalance</p>
-          <p>Filecoin.EthGetBalance</p>
-          <p>eth_gasPrice</p>
-          <p>Filecoin.EthGasPrice</p>
-          <p>eth_chainId</p>
-          <p>Filecoin.EthChainId</p>
-          <p>eth_getBlockTransactionCountByNumber</p>
-          <p>Filecoin.EthGetBlockTransactionCountByNumber</p>
-          <p>net_version</p>
-          <p>Filecoin.NetVersion</p>
-          <p>net_listening</p>
-          <p>Filecoin.NetListening</p>
-          <p>eth_protocolVersion</p>
-          <p>Filecoin.EthProtocolVersion</p>
-          <p>eth_maxPriorityFeePerGas</p>
-          <p>Filecoin.EthMaxPriorityFeePerGas</p>
-          <p>eth_getBlockByHash</p>
-          <p>Filecoin.EthGetBlockByHash</p>
-          <p>eth_getBlockTransactionCountByHash</p>
-          <p>Filecoin.EthGetBlockTransactionCountByHash</p>
-          <p>eth_getCode</p>
-          <p>Filecoin.EthGetCode</p>
-          <p>eth_getStorageAt</p>
-          <p>Filecoin.EthGetStorageAt</p>
-          <p>eth_sendRawTransaction</p>
-          <p>Filecoin.EthSendRawTransaction</p>
           <p>eth_call</p>
-          <p>Filecoin.EthCall</p>
+          <p>eth_chainId</p>
           <p>eth_estimateGas</p>
+          <p>eth_feeHistory</p>
+          <p>eth_gasPrice</p>
+          <p>eth_getBalance</p>
+          <p>eth_getBlockByHash</p>
+          <p>eth_getBlockByNumber</p>
+          <p>eth_getBlockTransactionCountByHash</p>
+          <p>eth_getBlockTransactionCountByNumber</p>
+          <p>eth_getCode</p>
+          <p>eth_getFilterChanges</p>
+          <p>eth_getFilterLogs</p>
+          <p>eth_getLogs</p>
+          <p>eth_getStorageAt</p>
+          <p>eth_getTransactionByBlockHashAndIndex</p>
+          <p>eth_getTransactionByBlockNumberAndIndex</p>
+          <p>eth_getTransactionByHash</p>
+          <p>eth_getTransactionCount</p>
+          <p>eth_getTransactionReceipt</p>
+          <p>eth_maxPriorityFeePerGas</p>
+          <p>eth_newBlockFilter</p>
+          <p>eth_newFilter</p>
+          <p>eth_newPendingTransactionFilter</p>
+          <p>eth_protocolVersion</p>
+          <p>eth_sendRawTransaction</p>
+          <p>eth_subscribe</p>
+          <p>eth_uninstallFilter</p>
+          <p>eth_unsubscribe</p>
+          <p>net_listening</p>
+          <p>net_version</p>
+          <p>Filecoin.EthAccounts</p>
+          <p>Filecoin.EthBlockNumber</p>
+          <p>Filecoin.EthCall</p>
+          <p>Filecoin.EthChainId</p>
           <p>Filecoin.EthEstimateGas</p>
+          <p>Filecoin.EthFeeHistory</p>
+          <p>Filecoin.EthGasPrice</p>
+          <p>Filecoin.EthGetBalance</p>
+          <p>Filecoin.EthGetBlockByHash</p>
+          <p>Filecoin.EthGetBlockByNumber</p>
+          <p>Filecoin.EthGetBlockTransactionCountByHash</p>
+          <p>Filecoin.EthGetBlockTransactionCountByNumber</p>
+          <p>Filecoin.EthGetCode</p>
+          <p>Filecoin.EthGetFilterChanges</p>
+          <p>Filecoin.EthGetFilterLogs</p>
+          <p>Filecoin.EthGetLogs</p>
+          <p>Filecoin.EthGetStorageAt</p>
+          <p>Filecoin.EthGetTransactionByBlockHashAndIndex</p>
+          <p>Filecoin.EthGetTransactionByBlockNumberAndIndex</p>
+          <p>Filecoin.EthGetTransactionByHash</p>
+          <p>Filecoin.EthGetTransactionCount</p>
+          <p>Filecoin.EthGetTransactionReceipt</p>
+          <p>Filecoin.EthMaxPriorityFeePerGas</p>
+          <p>Filecoin.EthNewBlockFilter</p>
+          <p>Filecoin.EthNewFilter</p>
+          <p>Filecoin.EthNewPendingTransactionFilter</p>
+          <p>Filecoin.EthProtocolVersion</p>
+          <p>Filecoin.EthSendRawTransaction</p>
+          <p>Filecoin.EthSubscribe</p>
+          <p>Filecoin.EthUninstallFilter</p>
+          <p>Filecoin.EthUnsubscribe</p>
+          <p>Filecoin.NetListening</p>
+          <p>Filecoin.NetVersion</p>
         </OneColumn>
       </TwoColumns>
     </NodePage>
