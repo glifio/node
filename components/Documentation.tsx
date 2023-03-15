469320ad1b81b2fd281705389e5f1d0611e09539
lazavikmaria <lazavikmaria@gmail.com>
2023-03-15 21:54:37 +0400

    change text to static, add filecoin rpc page

--- components/Documentation.tsx
+++ components/Documentation.tsx
@@ -7,8 +7,6 @@ import {
   ButtonV2Link,
   SmartLink
 } from '@glif/react-components'
-import { TitleWithSubtitle } from './Helpers'
-
 export default function Documentation() {
   return (
     <TwoColumns>
@@ -143,13 +141,6 @@ export default function Documentation() {
       </OneColumn>
 
       <OneColumn>
-        <TitleWithSubtitle>
-          <h2>Available Lotus Gateway API request methods</h2>
-          <SmartLink href='https://github.com/filecoin-project/lotus/blob/master/gateway/node.go#L45-L91'>
-            See Lotus Gateway API documentation
-          </SmartLink>
-        </TitleWithSubtitle>
-
         <WarningBox>
           Before going to Available API request methods, please, consider that{' '}
           <b>Filecoin.StateMarketDeals</b> operation data is available as a
@@ -177,110 +168,6 @@ export default function Documentation() {
             See the dedicated issue
           </SmartLink>
         </WarningBox>
-
-        <TitleWithSubtitle>
-          <h2>Available API request methods</h2>
-          <SmartLink href='https://lotus.filecoin.io/developers/apis/json-rpc/'>
-            See Lotus JSON-RPC API documentation
-          </SmartLink>
-        </TitleWithSubtitle>
-
-        <InfoBox>
-          All JSON-RPC requests must include the header:
-          <br />
-          <b>&quot;Content-Type&quot;: &quot;application/json&quot;</b>
-        </InfoBox>
-
-        <p>Filecoin.ChainGetBlockMessages</p>
-        <p>Filecoin.ChainGetGenesis</p>
-        <p>Filecoin.ChainGetMessage</p>
-        <p>Filecoin.ChainGetNode</p>
-        <p>Filecoin.ChainGetParentMessages</p>
-        <p>Filecoin.ChainGetParentReceipts</p>
-        <p>Filecoin.ChainGetPath</p>
-        <p>Filecoin.ChainGetTipSet</p>
-        <p>Filecoin.ChainGetTipSetAfterHeight</p>
-        <p>Filecoin.ChainGetTipSetByHeight</p>
-        <p>Filecoin.ChainHasObj</p>
-        <p>Filecoin.ChainHead</p>
-        <p>Filecoin.ChainNotify</p>
-        <p>Filecoin.ChainPutObj</p>
-        <p>Filecoin.ChainReadObj</p>
-        <p>Filecoin.Discover</p>
-        <p>Filecoin.EthAccounts</p>
-        <p>Filecoin.EthBlockNumber</p>
-        <p>Filecoin.EthCall</p>
-        <p>Filecoin.EthChainId</p>
-        <p>Filecoin.EthEstimateGas</p>
-        <p>Filecoin.EthFeeHistory</p>
-        <p>Filecoin.EthGasPrice</p>
-        <p>Filecoin.EthGetBalance</p>
-        <p>Filecoin.EthGetBlockByHash</p>
-        <p>Filecoin.EthGetBlockByNumber</p>
-        <p>Filecoin.EthGetBlockTransactionCountByHash</p>
-        <p>Filecoin.EthGetBlockTransactionCountByNumber</p>
-        <p>Filecoin.EthGetCode</p>
-        <p>Filecoin.EthGetFilterChanges</p>
-        <p>Filecoin.EthGetFilterLogs</p>
-        <p>Filecoin.EthGetLogs</p>
-        <p>Filecoin.EthGetMessageCidByTransactionHash</p>
-        <p>Filecoin.EthGetStorageAt</p>
-        <p>Filecoin.EthGetTransactionByHash</p>
-        <p>Filecoin.EthGetTransactionCount</p>
-        <p>Filecoin.EthGetTransactionHashByCid</p>
-        <p>Filecoin.EthGetTransactionReceipt</p>
-        <p>Filecoin.EthMaxPriorityFeePerGas</p>
-        <p>Filecoin.EthNewBlockFilter</p>
-        <p>Filecoin.EthNewFilter</p>
-        <p>Filecoin.EthNewPendingTransactionFilter</p>
-        <p>Filecoin.EthProtocolVersion</p>
-        <p>Filecoin.EthSendRawTransaction</p>
-        <p>Filecoin.EthSubscribe</p>
-        <p>Filecoin.EthUninstallFilter</p>
-        <p>Filecoin.EthUnsubscribe</p>
-        <p>Filecoin.GasEstimateGasPremium</p>
-        <p>Filecoin.GasEstimateMessageGas</p>
-        <p>Filecoin.MpoolGetNonce</p>
-        <p>Filecoin.MpoolPush</p>
-        <p>Filecoin.MsigGetAvailableBalance</p>
-        <p>Filecoin.MsigGetPending</p>
-        <p>Filecoin.MsigGetVested</p>
-        <p>Filecoin.MsigGetVestingSchedule</p>
-        <p>Filecoin.NetListening</p>
-        <p>Filecoin.NetVersion</p>
-        <p>Filecoin.StateAccountKey</p>
-        <p>Filecoin.StateCall</p>
-        <p>Filecoin.StateCirculatingSupply</p>
-        <p>Filecoin.StateDealProviderCollateralBounds</p>
-        <p>Filecoin.StateDecodeParams</p>
-        <p>Filecoin.StateGetActor</p>
-        <p>Filecoin.StateListMiners</p>
-        <p>Filecoin.StateLookupID</p>
-        <p>Filecoin.StateMarketBalance</p>
-        <p>Filecoin.StateMarketStorageDeal</p>
-        <p>Filecoin.StateMinerAvailableBalance</p>
-        <p>Filecoin.StateMinerDeadlines</p>
-        <p>Filecoin.StateMinerFaults</p>
-        <p>Filecoin.StateMinerInfo</p>
-        <p>Filecoin.StateMinerPower</p>
-        <p>Filecoin.StateMinerProvingDeadline</p>
-        <p>Filecoin.StateMinerRecoveries</p>
-        <p>Filecoin.StateMinerSectorCount</p>
-        <p>Filecoin.StateNetworkName</p>
-        <p>Filecoin.StateNetworkVersion</p>
-        <p>Filecoin.StateReadState</p>
-        <p>Filecoin.StateReplay</p>
-        <p>Filecoin.StateSearchMsg</p>
-        <p>Filecoin.StateSectorGetInfo</p>
-        <p>Filecoin.StateVerifiedClientStatus</p>
-        <p>Filecoin.StateVerifierStatus</p>
-        <p>Filecoin.StateVMCirculatingSupplyInternal</p>
-        <p>Filecoin.StateWaitMsg</p>
-        <p>Filecoin.Version</p>
-        <p>Filecoin.WalletBalance</p>
-        <p>Filecoin.WalletVerify</p>
-        <p>Filecoin.Web3ClientVersion</p>
-        <p>rpc.discover</p>
       </OneColumn>
     </TwoColumns>
   )
